70cba99df163ed8e0bf51430fa50b4480179280c
Son Smith <son10c3k56@gmail.com>
2023-05-18 17:18:46 +0700

    add more fucntion

--- data_structor/Array_Ds.py
+++ data_structor/Array_Ds.py
@@ -13,7 +13,7 @@ import sys
 # The function accepts INTEGER_ARRAY a as parameter.
 #
 
-def reverseArray(a):
+def reverseArray(a): # gọi là slicing 
     # Write your code here
     """
     sử dụng hàm slicing để đảo ngược mảng
@@ -21,15 +21,33 @@ def reverseArray(a):
     -1 là bước nhảy-> mảng sẽ cắt từng phần tử từ phần tử cuối cùng có index -1 cho đến hết
     """
     return a[::-1]
+def reverse_array2(arr): # cách này là dùng vòng lặp
+    start = 0
+    end = len(arr) - 1
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr
+
+def reverse_array(arr):# Dùng đệ quy
+    if len(arr) <= 1:
+        return arr
+    return reverse_array(arr[1:]) + [arr[0]]
+
 
 if __name__ == '__main__':
     
-
+    #Ví dụ cho input:
+    #5
+    #1 2 3 4 5
     arr_count = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
 
-    res = reverseArray(arr)
+    # res = reverse_array2(arr)
+    res = reverse_array(arr)
+    # res = reverseArray(arr)
     print("result: ",res)
 
     
--- data_structor/hour_glass.py
+++ data_structor/hour_glass.py
@@ -26,12 +26,43 @@ def hourglassSum(arr):
             max_sum = max(max_sum, current_sum)  # Cập nhật giá trị max_sum
 
     return max_sum
+
+def hourglass_sum(arr):
+    n = len(arr)
+    m = len(arr[0])
+    sums = [[0] * m for _ in range(n)]
+
+    # Tính tổng các giá trị từ góc trái trên đến vị trí hiện tại
+    for i in range(n):
+        for j in range(m):
+            sums[i][j] = arr[i][j] + (
+                sums[i-1][j] if i > 0 else 0) + (
+                sums[i][j-1] if j > 0 else 0) - (
+                sums[i-1][j-1] if i > 0 and j > 0 else 0)
+
+    max_sum = float('-inf')
+
+    # Duyệt qua từng vị trí và tính tổng hourglass
+    for i in range(2, n):
+        for j in range(2, m):
+            hourglass_sum = sums[i][j] - sums[i-2][j] - sums[i][j-2] + sums[i-2][j-2]
+            max_sum = max(max_sum, hourglass_sum)
+
+    return max_sum
+
 if __name__ == '__main__':
     arr = []
-
+    #Example for input:
+    # 1 1 1 0 0 0
+    # 0 1 0 0 0 0
+    # 1 1 1 0 0 0
+    # 0 0 2 4 4 0
+    # 0 0 0 2 0 0
+    # 0 0 1 2 4 0
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    result = hourglassSum(arr)
+    # result = hourglassSum(arr)
+    result = hourglass_sum(arr)
     print("result: ",result)
 
